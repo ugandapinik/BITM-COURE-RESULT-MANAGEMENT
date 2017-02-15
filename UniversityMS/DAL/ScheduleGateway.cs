9bf7ef13c2595d99574cc5e8e5735d9dd0953952
wohhie <wohhie92@gmail.com>
2017-02-15 12:35:14 +0600

    Updated

---
+++ UniversityMS/DAL/ScheduleGateway.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace UniversityMS.DAL {
+    public class ScheduleGateway {
+
+
+        public string connectionString =
+            WebConfigurationManager.ConnectionStrings["UniversityDbContext"].ConnectionString;
+
+
+        public bool IsDepartmentNameExist(string DeptName) {
+            bool isDeptNameExists = false;
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            string query = "SELECT DeptName FROM Departments WHERE DeptName= @DeptName ";
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.Clear();
+
+            command.Parameters.Add("DeptName", SqlDbType.NVarChar);
+            command.Parameters["DeptName"].Value = DeptName;
+
+
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            if (reader.Read()) {
+                isDeptNameExists = true;
+            }
+            connection.Close();
+
+            return isDeptNameExists;
+        }
+
+    }
+}
